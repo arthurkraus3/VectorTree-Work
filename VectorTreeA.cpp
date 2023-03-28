2518e645896795d114e1a13d4ccc214fc6120a47
Arthur Kraus <akraus3@luc.edu>
2023-03-28 16:20:29 -0500

    March28Upload
    AutoFlush off, one branch with 1000 entries (size 4 bytes) with a fraction of randomized values.

--- VectorTreeA.cpp
+++ VectorTreeA.cpp
@@ -18,6 +18,9 @@ void VectorTree() {
     
     My goal is to separately load up four different branches with different vector sizes
     
+        Size 1: 1 entry
+        Size 2: 10 entries
+        Size 3: 100 entries
         Size 4: 1000 entries    
     
     */
@@ -43,22 +46,34 @@ void VectorTree() {
   std::cout << "initializing variables and vector" << std::endl;
   
   const Int_t NEvents = 1000000; // N = 1,000,000
+  Int_t NEntries0;
+  Int_t NEntries1;
+  Int_t NEntries2;
   Int_t NEntries3;
   
       //vectors
+      std::vector<float> vten0; //10^0 = 1 entry
+      std::vector<float> vten1;//10^1 = 10 entries
+      std::vector<float> vten2;//10^2 = 100 entries
       std::vector<float> vten3;// 10^3 = 1000 entries
       
   
       //variables
+      float f0;
+      float f1;
+      float f2;
       float f3;
   
   
   //INITIALIZING BRANCHES
   std::cout << "creating branches" << std::endl;
   
+  tree->Branch("bOnes", &vten0);
+  tree->Branch("bTens", &vten1);
+  tree->Branch("bHundreds", &vten2);
   tree->Branch("bThousands", &vten3);
   
-  int bSize = 8192000;
+  int bSize = 4000;
 
   tree->SetBasketSize("*",bSize);
   
@@ -68,17 +83,46 @@ void VectorTree() {
   for (int j = 0; j < NEvents; j++) {
     
     //randomizing number of entries
+    int NEntries0 = 1;
+    int NEntries1 = 10;
+    int NEntries2 = 100;
     int NEntries3 = 1000;
    
     //clearing events
+    vten0.clear();
+    vten1.clear();
+    vten2.clear();
     vten3.clear();
           
         //generating vector elements, filling vectors
         
+        //generating vten0
+        for(int m = 0; m < NEntries0; m++) {
+          if(m < (NEntries0 / 2)){
+		f0 = gRandom->Gaus(0,1) * gRandom->Rndm();
+         	 vten0.emplace_back(f0);
+	} else {
+		f0 = m; 
+		vten0.emplace_back(f0);
+	}
+        }
+    
+        //generating vten1
+        for(int n = 0; n < NEntries1; n++) {
+          if(n < NEntries1/2) {
+		f1 = gRandom->Rndm() * gRandom->Gaus(0,1);
+          	vten1.emplace_back(f1);
+        } else {
+		f1 = 1; 
+		vten1.emplace_back(f1);	
+	}
+	
+	}
+    
     
         //generating vten3
         for(int a = 0; a < NEntries3; a++) {
-	if(a < NEntries3/5) { 
+	if(a < NEntries3/2) { 
           	f3= gRandom->Rndm() * gRandom->Gaus(0,1);
           	vten3.emplace_back(f3);
 	} else  { 
