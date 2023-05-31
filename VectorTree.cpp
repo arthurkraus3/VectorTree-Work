fd90740de45d1bd24228c60b2d93f25da2146033
Arthur Kraus <akraus3@luc.edu>
2023-05-31 10:07:43 -0500

    May 31st Update to VectorTree.cpp

--- VectorTree.cpp
+++ VectorTree.cpp
@@ -2,6 +2,7 @@
 #include <ostream>
 #include <memory>
 #include <vector>
+#include <cmath>
 
 #include "TFile.h"
 #include "TTree.h"
@@ -11,7 +12,27 @@
 #include "TStyle.h"
 #include "TCanvas.h"
 
-void VectorTree() {
+union floatint_t {
+     float fvalue;
+     uint32_t ivalue;
+  };  
+
+uint32_t getBitMask(int lastNBits) {
+    uint32_t bitmask = 0;
+    for(auto i = 0; i < lastNBits; ++i ) { 
+      bitmask |= (0x1 << i); 
+    }   
+    return ~bitmask;
+  }
+
+float truncate(float input, int lastNBits) {
+    ::floatint_t value;
+    value.fvalue = input; 
+    value.ivalue &= ::getBitMask(lastNBits);
+    return value.fvalue;
+  }
+
+void VectorTree(int n) {
   //Credit for the bones to this code goes to Alexei Tam
 
     /*
@@ -32,8 +53,8 @@ void VectorTree() {
   std::unique_ptr<TFile> myFile = std::make_unique<TFile>("VectorTreeFile.root", "RECREATE");
   TTree *tree = new TTree("myTree","myTree");
  
-  tree->SetAutoFlush(0);
-  tree->SetAutoSave(0);
+ // tree->SetAutoFlush(0);
+ // tree->SetAutoSave(0);
 
   if(!myFile) {
     std::cout << "Error: file creation failed" << std::endl;
@@ -57,54 +78,52 @@ void VectorTree() {
   std::cout << "creating branches" << std::endl;
   
   tree->Branch("bThousands", &vten3);
-  
-  int bSize = 8192000;
+
+  int power = pow(2,n);
+
+  int bSize = power*4000;
 
   tree->SetBasketSize("*",bSize);
   
-  //EVENTS LOOP
+  //===EVENTS LOOP===
   std::cout << "generating events..." << std::endl;
   
   for (int j = 0; j < NEvents; j++) {
     
     //randomizing number of entries
     int NEntries3 = 1000;
-   
+    ::floatint_t truncated;
+    
     //clearing events
     vten3.clear();
           
-        //generating vector elements, filling vectors
-        
-    
-        //generating vten3
-        for(int a = 0; a < NEntries3; a++) {
-	if(a < NEntries3/5) { 
-          	f3= gRandom->Rndm() * gRandom->Gaus(0,1);
-          	vten3.emplace_back(f3);
+    //generating vten3
+    for(int a = 0; a < NEntries3; a++) {
+	if(a < NEntries3/2) { 
+       		f3= gRandom->Rndm() * gRandom->Gaus(0,1);
+       		truncated.fvalue = ::truncate(f3,15); // 8 for 16 dp, 15 for 8 dp.
+		vten3.emplace_back(truncated.fvalue);
 	} else  { 
 		f3 = 1;
 		vten3.emplace_back(f3); 
 	}
-        }
-    
-        
-    tree->Fill();
-    
+    }
+    tree->Fill(); 
   }
   
   //saving tree and file
-  tree->Write();
-  
-    //let's look in the tree:
-    //tree->Scan();
-    tree->Print();
-  
+  tree->Write(); 
+  //let's look in the tree:
+  //tree->Scan();
+  tree->Print();
   myFile->Save();
   myFile->Close();
- 
 }
 
 int main(){
-	VectorTree();
+	for(int i = 1; i < 13;i++){
+		VectorTree(i);
+	}
+
 return 0;
 }
